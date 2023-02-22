e3339774f989966c9b0bfb9aceb790c9d84e4d07
GuillermoSamboni <guillermosamboni0912@gmail.com>
2023-02-21 19:56:13 -0500

    vercel.json x2

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
